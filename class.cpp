8d3882d1b278ece05802849632207a5cf56aed36
JaCKoP619 <jakub007vip@gmail.com>
2023-07-14 20:40:19 +0200

    added file for unit class

---
+++ class.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+class Unit{
+public:
+Unit(char type) {
+
+};
+
+private:
+
+const int hpMax;    // max hp points for given unit type
+int hpCurr;         // current hp point of the unit
+
+const int spd;      //speed of the unit
+};
